31eacd006f76f9415aae51c4167d3d4215c4f826
Santiago Paiz <santiago.paiz@gmail.com>
2023-03-26 09:59:48 -0400

    first commit

---
+++ DietService.py
@@ -0,0 +1,40 @@
+def model(prob,day,kg,calories):
+    G = extract_gram(build_nutritional_values(kg,calories))
+    E = G['Carbohydrates Grams']
+    F = G['Fat Grams']
+    P = G['Protein Grams']
+    day_data = days_data[day]
+    day_data = day_data[day_data.calories!=0]
+    food = day_data.name.tolist()
+    c  = day_data.calories.tolist()
+    x  = pulp.LpVariable.dicts( "x", indices = food, lowBound=0, upBound=1.5, cat='Continuous', indexStart=[] )
+    e = day_data.carbohydrate.tolist()
+    f = day_data.total_fat.tolist()
+    p = day_data.protein.tolist()
+#    prob  = pulp.LpProblem( "Diet", LpMinimize )
+    prob += pulp.lpSum( [x[food[i]]*c[i] for i in range(len(food))]  )
+    prob += pulp.lpSum( [x[food[i]]*e[i] for i in range(len(x)) ] )>=E
+    prob += pulp.lpSum( [x[food[i]]*f[i] for i in range(len(x)) ] )>=F
+    prob += pulp.lpSum( [x[food[i]]*p[i] for i in range(len(x)) ] )>=P
+    prob.solve()
+    variables = []
+    values = []
+    for v in prob.variables():
+        variable = v.name
+        value = v.varValue
+        variables.append(variable)
+        values.append(value)
+    values = np.array(values).round(2).astype(float)
+    sol = pd.DataFrame(np.array([food,values]).T, columns = ['Food','Quantity'])
+    sol['Quantity'] = sol.Quantity.astype(float)
+    sol = sol[sol['Quantity']!=0.0]
+    sol.Quantity = sol.Quantity*100
+    sol = sol.rename(columns={'Quantity':'Quantity (g)'})
+    return sol
+def total_model(kg,calories):
+    result = []
+    for day in week_days:
+        prob  = pulp.LpProblem( "Diet", LpMinimize )
+        print('Building a model for day %s \n'%(day))
+        result.append(model(prob,day,kg,calories))
+    return dict(zip(week_days,result))
